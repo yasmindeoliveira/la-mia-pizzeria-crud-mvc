0fc7b56e771526fff10c1c96c9b59bf872584a7a
yasmindeoliveira <yaz.oliveira01@gmail.com>
2023-01-17 16:12:36 +0100

    Add project files.

---
+++ LaMiaPizzeriaEfPost/Controllers/PizzaController.cs
@@ -0,0 +1,128 @@
+﻿using LaMiaPizzeriaEfPost.Database;
+using LaMiaPizzeriaEfPost.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace LaMiaPizzeriaModel.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Home() 
+        {
+            return View(); 
+        }
+        public IActionResult Index()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizzas= db.Pizzas.ToList<Pizza>();
+                return View("Index", pizzas);
+            }
+   
+        }
+
+        public IActionResult Dettagli(string nome)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza pizza = (from p in db.Pizzas
+                               where p.nome == nome
+                               select p).FirstOrDefault();
+
+                if (pizza != null)
+                {
+                    return View(pizza);
+                }
+
+                return NotFound("La pizza con il nome cercato non è disponibile");
+            }
+            
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", formData);
+            }
+
+            using (PizzaContext db = new PizzaContext())
+            {
+                db.Pizzas.Add(formData);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(string nome)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza pizza = (from p in db.Pizzas
+                               where p.nome == nome
+                               select p).FirstOrDefault();
+
+                if (pizza != null)
+                {
+                    return View(pizza);
+                }
+
+                return NotFound("La pizza con il nome cercato non è disponibile");
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Pizza formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", formData);
+            }
+
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza pizza = (from p in db.Pizzas
+                               where p.nome == formData.nome
+                               select p).FirstOrDefault();
+
+                pizza.descrizione = formData.descrizione;
+                pizza.prezzo = formData.prezzo;
+                pizza.foto = formData.foto;
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Remove(string nome)
+        {
+
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza pizza = (from p in db.Pizzas
+                               where p.nome == nome
+                               select p).FirstOrDefault();
+
+                db.Remove(pizza);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ LaMiaPizzeriaEfPost/Models/Pizza.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaMiaPizzeriaEfPost.Models
+{
+    public class Pizza
+    {
+  
+        [Key]
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string nome { get; set; }
+
+        [Url]
+        [Required]
+        [Column(TypeName = "varchar(300)")]
+        public string foto { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        public string descrizione { get; set; }
+
+        [Required]
+        [Range(70,15000)]
+        public double prezzo { get; set; }
+
+        public Pizza()
+        {
+
+        }
+        public Pizza(string nome, string foto, string descrizione, double prezzo)
+        {
+            this.nome = nome;
+            this.foto = foto;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+        }
+    }
+}
